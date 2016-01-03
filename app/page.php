52eb7d0f315c89f2f417a9b370cbcb71b4bbb927
dannyng198811 <danny.ng198811@yahoo.com>
2016-01-03 18:47:05 +0800

    dsds
    dsds

--- app/page.php
+++ app/page.php
@@ -27,23 +27,48 @@ require_once 'init.php';
     <div class="left_sidebar sidebar clearfix">
       
       <section>
-        <h3>簡介</h3>
-        <p>Maecenas quis facilisis odio. <a href="#">Nullam egestas</a> velit gravida libero rutrum at elementum velit tincidunt. In quis suscipit nunc. Curabitur quis est felis.</p>
-        <p>Donec sit amet justo nisi. Sed porttitor euismod metus, in bibendum dolor laoreet id. Aliquam vel nulla non turpis rhoncus ultrices.</p>
+        <h3>治療價目表</h3>
+          <ul class="price">
+            <li>開藥 100元</li>
+              <li>針灸 300元</li>
+<li>濕疹微電治療 300⾄至1400元<br/>
+    (視乎濕疹面積及治療時間長短)</li>
+              <li>上門治療 1000至2000元</li>
+              <li>經筋鬆解推拿治療 400元/50分鐘</li>
+              <li>紅外線照燈治療 150元/25分鐘</li>
+              <li>微電貼治療 100元</li>
+              <li>耳穴治療 150元</li>
+              <li>中醫纖體治療 700元</li>
+<li>中醫面部肌膚改善治療 700元</li>
+          </ul>
+        
       </section>
       
 
       <section>
-        <h3>服務項目</h3>
-        <p>Maecenas quis facilisis odio. Nullam egestas velit gravida libero rutrum at elementum velit tincidunt.</p>
-        <ul class="no_bullets">
-          <li><i class="icon-ok-circle"></i> Lorem ipsum dolor sit</li>
-          <li><i class="icon-time"></i> Maecenas quis facilisis odio</li>
-          <li><i class="icon-upload"></i> Curabitur quis est felis</li>
-          <li><i class="icon-cog"></i> Donec sit amet</li>
-          
-        </ul>
+        <h3>藥物及產品價目表</h3>
+        <ul class="price">
+            <li>全新微電貼 每對100元</li>
+          <li>皮膚藥膏 150丶250 或 350元<br/>
+            (治療濕疹，絶對不含類固醇)</li>
+            <li>浸浴藥粉 150⼂丶250 或 350元</li>
+            <li>藏紅花 每瓶180元</li>
+            <li>微電機租借之按金 1800元</li>
+            <li>微電機租借之月費 450元</li>
+          </ul>
+       
       </section>
+        
+        <section class="about">
+        <h3>黎凱明註册中醫<br/><br/>
+內科·婦科·兒科·皮膚·運動創傷</h3>
+        <p>香港銅鑼灣希慎道8號裕景商業中心2樓</p>
+            <p><label>Tel : </label>(852) 2889 6080 </p>
+            <p><label>Mobile : </label>(852) 9020 8382</p>
+            <p><label>acebook : </label>黎凱明中醫診所</p>
+             <h3><a href="#">敬請預約 >> </a></h3>
+      </section>
+
 
     </div>
 
@@ -53,7 +78,9 @@ require_once 'init.php';
 
       <!-- Features -->
       <section>
-
+       <ol>
+         <li>1.經筋鬆解推拿療法</li>   
+       </ol>
         <ul class="features_icons features">
           <li>
             <i class="icon-fullscreen black big"></i>
